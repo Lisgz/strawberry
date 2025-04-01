28d3972ad7545f91613dcf1a96702d0ebc45d1aa
Lisgz <lismariegomez1@gmail.com>
2025-04-01 12:57:58 -0500

    Update 121-converion.py

--- L/121-converion.py
+++ L/121-converion.py
@@ -5,5 +5,5 @@ print("You have " + str(points) + " to start.")
 #an error messsage will pop up if you combine an interger with a string (my note)
 
 #review: converting data types is necessary to make data usable in a situation.
-#Data captured via inputs are strings of fata, so if we use input data in numeric calculations, the data needs to be converted to a number.
+#Data captured via inputs are strings of data, so if we use input data in numeric calculations, the data needs to be converted to a number.
 #finally, python cannot convert what is not possible such as a string, entered as a float to an integar, at least not through the int function.
